dd82e4b61f49e20366e6fbfc261c9553949f0189
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-10-17 18:53:59 -0500

    Add files via upload

---
+++ RTF/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RTF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(204, 87);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(176, 47);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Abrir";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(525, 87);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(176, 47);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(80, 182);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(751, 252);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(204, 494);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(176, 47);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(525, 493);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(176, 48);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Borrar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ArchivoRTF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 599);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RTF/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RTF
+{
+    public partial class Form1 : Form
+    {
+        private string rtfFilePath; // Ruta del archivo RTF
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            // Mostrar el cuadro de diálogo de apertura de archivo
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos RTF|*.rtf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                rtfFilePath = openFileDialog.FileName;
+                LoadAndDisplayRtfData();
+                MessageBox.Show("Archivo RTF cargado exitosamente.");
+            }
+        }
+
+        private void LoadAndDisplayRtfData()
+        {
+            try
+            {
+                // Leer todos los datos del archivo RTF y mostrarlos en un cuadro de texto
+                using (FileStream fileStream = new FileStream(rtfFilePath, FileMode.Open))
+                {
+                    richTextBox1.LoadFile(fileStream, RichTextBoxStreamType.RichText);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar y mostrar desde el archivo RTF: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Guardar el contenido del RichTextBox txtContent de vuelta al archivo RTF
+                using (FileStream fileStream = new FileStream(rtfFilePath, FileMode.Open))
+                {
+                    richTextBox1.SaveFile(fileStream, RichTextBoxStreamType.RichText);
+                }
+
+                MessageBox.Show("Guardado en el archivo RTF.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar en el archivo RTF: " + ex.Message);
+            }
+        }
+
+        // Evento para el botón "Editar"
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay algo seleccionado en el RichTextBox
+            if (!string.IsNullOrWhiteSpace(richTextBox1.SelectedText))
+            {
+                // Obtener el texto seleccionado
+                string selectedText = richTextBox1.SelectedText;
+
+                // Mostrar un cuadro de diálogo para editar el texto seleccionado
+                string editedText = Microsoft.VisualBasic.Interaction.InputBox("Editar texto:", "Edición", selectedText);
+
+                // Reemplazar el texto seleccionado con el texto editado
+                richTextBox1.Text = richTextBox1.Text.Replace(selectedText, editedText);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el texto que desea editar.");
+            }
+        }
+
+        // Evento para el botón "Borrar"
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay algo seleccionado en el RichTextBox
+            if (!string.IsNullOrWhiteSpace(richTextBox1.SelectedText))
+            {
+                // Borrar el texto seleccionado
+                richTextBox1.Text = richTextBox1.Text.Replace(richTextBox1.SelectedText, "");
+            }
+            else
+            {
+                MessageBox.Show("Seleccione el texto que desea borrar.");
+            }
+        }
+    }
+}
